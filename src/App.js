b7b9b09bce1803e394b271dbd6aa3f366cc08afa
mrmurtz <murtaza.s.abidi@gmail.com>
2018-06-22 14:42:33 +0100

    Finished keypress and handlechanges next step to link to giffy API

--- src/App.js
+++ src/App.js
@@ -6,26 +6,60 @@ const Header = () => (
   </div>
 )
 
+const UserHint = ({loading, hintText}) => (
+  <div className='user-hint'>
+    {loading ? <img className='block mx-auto' /> : hintText}
+  </div>
+
+)
+
 class App extends Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      searchTerm: '',
+      hintText: ''
+    }
+  }
 
   handleChange = event => {
     // below is same as const value = event.target.value
     const {value} = event.target
-    console.log(value)
+    this.setState((prevState, props) => ({
+      ...prevState,
+      searchTerm: value,
+      hintText: value.length > 2 ? `Hit enter to search ${value}` : ''
+    }))
+  };
+
+  handleKeyPress = event => {
+    const {value} = event.target;
+    if (value.length > 2 && event.key === 'Enter') {
+      alert(`Search for ${value}`)
+    }
   };
 
   render() {
+    const{searchTerm} = this.state;
     return (
       <div className="page">
         <Header />
         <div className="search grid">
 
         <input className="input grid-item" placeholder="Type something"
-        onChange={this.handleChange}/>
+        onChange={this.handleChange}
+        onKeyPress= {this.handleKeyPress}
+        value={searchTerm}
+        />
         </div>
+        <UserHint {...this.state} />
       </div>
     );
   }
 }
+
+
 
 export default App;
